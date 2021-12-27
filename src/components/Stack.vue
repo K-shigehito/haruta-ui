245a0d6ed465a0076b4f92a275f6544e5eeb2cde
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2021-12-27 23:28:57 +0900

    add: Stackコンポーネントの追加 #1

---
+++ src/components/Stack.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from '@vue/reactivity';
+
+interface Props {
+  space?: number;
+}
+const props = defineProps<Props>();
+const spaceStyle = computed(() =>
+  props.space ? { gap: `${props.space}em` } : ''
+);
+</script>
+
+<template>
+  <div class="flex flex-col justify-start" :style="spaceStyle">
+    <slot />
+  </div>
+</template>
+
+<style>
+/* .stack * + * {
+  margin-top: 1.7rem;
+} */
+</style>
